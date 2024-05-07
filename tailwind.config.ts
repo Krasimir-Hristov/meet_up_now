e30abf6262d33e3293ebc10462a9c284ffe26b04
krasi <krasimir.xristov@gmail.com>
2024-05-07 18:32:30 +0200

    add test routes and add 2 more colors in tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,6 +23,10 @@ const config = {
           1: '#1C1F2E',
           2: '#161925',
         },
+        blue: {
+          1: '#0000CD',
+          2: '#191970',
+        },
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
         ring: 'hsl(var(--ring))',
