a69b09303972e9e57bda2fadc41feed6f8c45d9a
krasi <krasimir.xristov@gmail.com>
2024-05-19 16:38:01 +0200

    create room and ad the seting call options

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import './globals.css';
 import React from 'react';
 import { ClerkProvider } from '@clerk/nextjs';
 
+import '@stream-io/video-react-sdk/dist/css/styles.css';
+
 import { Toaster } from '@/components/ui/toaster';
 
 const inter = Inter({ subsets: ['latin'] });
